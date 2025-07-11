3159ea271a5c853870b09aa4ab60840e67a981c6
Michael Moore <mikefda12@gmail.com>
2025-07-11 17:33:37 -0400

    updated ui

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
 from flask import Flask, render_template, request, redirect, url_for, session, flash
 from models import db , job, User
+from functools import wraps
 import os
+
+#DECORATORS
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'user_id' not in session:
+            flash('You need to log in first!', 'warning')
+            return redirect(url_for('login'))
+        return f(*args, **kwargs)
+    return decorated_function
 
 app = Flask(__name__)
 app.secret_key = "mike371326moore888809"
@@ -13,10 +24,11 @@ with app.app_context():
     db.create_all()
 
 @app.route('/')
+@login_required
 def index():
-    if 'user_id' not in session:
-        flash('You need to log in first!', 'warning')
-        return redirect(url_for('login'))
+    # if 'user_id' not in session:
+    #     flash('You need to log in first!', 'warning')
+    #     return redirect(url_for('login'))
     
     search_query = request.args.get('search')
     status = request.args.get('status')
@@ -35,11 +47,30 @@ def index():
     jobs = job.query.all()
     return render_template('index.html', jobs=jobs)
 
+@app.route("/dashboard")
+@login_required
+def dashboard():
+    uid = session["user_id"]
+
+
+    total_jobs = job.query.filter_by(user_id=uid).count()
+    applied_jobs = job.query.filter_by(user_id=uid, status='Applied').count()
+    interviewing = job.query.filter_by(user_id=uid, status='Interviewing').count()
+    offer_received = job.query.filter_by(user_id=uid, status='Offer Received').count()
+    rejected = job.query.filter_by(user_id=uid, status='Rejected').count()
+
+    return render_template('dashboard.html',
+                           total_jobs=total_jobs,
+                           applied_jobs=applied_jobs,
+                           interviewing=interviewing,
+                           offer_received=offer_received,
+                           rejected=rejected)
+
+
+
 @app.route("/add", methods=["GET", "POST"])
+@login_required
 def add_job():
-    if 'user_id' not in session:
-        flash('You need to log in first!', 'warning')
-        return redirect(url_for('login'))
     
     if request.method == "POST":
         company = request.form.get("company")
@@ -54,6 +85,7 @@ def add_job():
         return redirect(url_for('index'))
 
     return render_template('add_job.html')
+
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
@@ -84,27 +116,24 @@ def login():
     return render_template('login.html')
 
 @app.route("/logout")
+@login_required
 def logout():
     session.pop('user_id', None)
     flash('You have been logged out.', 'info')
     return redirect(url_for('index'))
 
 @app.route("/delete/<int:job_id>", methods=["POST"])
+@login_required
 def delete_job(job_id):
-    if 'user_id' not in session:
-        flash('You need to log in first!', 'warning')
-        return redirect(url_for('login'))
-    
+     
     job_to_delete = job.query.get_or_404(job_id)
     db.session.delete(job_to_delete)
     db.session.commit()
     return redirect(url_for('index'))
 
 @app.route("/edit/<int:job_id>", methods=["GET", "POST"])
+@login_required
 def edit_job(job_id):
-    if 'user_id' not in session:
-        flash('You need to log in first!', 'warning')
-        return redirect(url_for('login'))
     
     job = job.query.get_or_404(job_id)
     if request.method == "POST":
